69b468096464e8d73bcec357a1ba4b86c6536d99
Aviral Janveja <janveja@mail.upb.de>
2019-06-25 20:31:19 +0200

    add first program file

---
+++ Programs/HelloWorld.java
@@ -0,0 +1,16 @@
+package helloworld;
+
+/**
+ * 
+ * @author AviralJanveja
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Can I Think?");
+    }
+    
+}
