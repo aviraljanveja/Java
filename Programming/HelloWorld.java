31ecfa8cfa27dcff7c724941adec30e36e80731b
Aviral Janveja <janveja@mail.upb.de>
2019-08-11 15:03:22 +0200

    modify hello world program

--- Programming/HelloWorld.java
+++ Programming/HelloWorld.java
@@ -1,16 +1,13 @@
-package helloworld;
-
 /**
- * 
  * @author AviralJanveja
+ *
  */
 public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Can I Think?");
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Can I Think?");
-    }
-    
+	}
+
 }
