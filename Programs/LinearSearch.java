1ef3a04459b5331e03606aac99bbd8c785f25161
Aviral Janveja <janveja@mail.upb.de>
2019-08-11 15:48:18 +0200

    change to Linear Search program

--- Programs/LinearSearch.java
+++ Programs/LinearSearch.java
@@ -1,10 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package linearsearch;
-
 /**
  *
  * @author AviralJanveja
@@ -21,9 +14,11 @@ public class LinearSearch {
     }
     
     public static void main(String[] args) {
+        
         int arr[] = {0,1,2,3,4,5,6,7,8,9};
         Scanner input = new Scanner(System.in);
         System.out.println("Please enter the number that you want to search : ");
+        
         int num = input.nextInt();
         int result = Linear_Search(num, arr);
         
