a5c8af458f4eba8fad6907e6eac46a1d407ef46f
Aviral Janveja <janveja@mail.upb.de>
2020-02-27 00:43:52 +0100

    Completes ch 4 operators.

--- Tutorials/Chapter 4 - Operators/28 - LeftShiftMultby2.java
+++ Tutorials/Chapter 4 - Operators/28 - LeftShiftMultby2.java
@@ -12,7 +12,9 @@ public class LeftShiftMultby2 {
 		
 		for (i=0; i<4; i++) {
 			num = num << 1;
-			System.out.println(num); // Left shifting the value a 4th time puts 1 in the high order bit.
+			System.out.println(num);
+			System.out.println(Integer.toBinaryString(num)); // Note that Zeroes on the Left High order bit positions are not printed by Java.
+			// Left shifting the value a 4th time puts 1 in the high order bit.
 			// Thus making the value negative instead of multiplying by 2. 
 		}
 
