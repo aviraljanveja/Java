b61dad63c378200f93b90fabce0599d6b0e460f7
Aviral Janveja <janveja@mail.upb.de>
2020-02-26 03:38:37 +0100

    directory structure change to chapter-wise and additional lessons for chapter 4, chapter 5 to follow.

---
+++ Tutorials/Chapter 4 - Operators/23 - LeftShiftMultby2.java
@@ -0,0 +1,21 @@
+/**
+ * @author AviralJanveja
+ * Left shifting as a quick way to multiply by 2, but with a Risk ! 
+ */
+public class LeftShiftMultby2 {
+
+	public static void main(String[] args) {
+		
+		int i;
+		int num = 268435454;
+		//int num = 0xFFFFFFE; (Same value as above, but in HexaDecimal.)
+		
+		for (i=0; i<4; i++) {
+			num = num << 1;
+			System.out.println(num); // Left shifting the value a 4th time puts 1 in the high order bit.
+			// Thus making the value negative instead of multiplying by 2. 
+		}
+
+	}
+
+}
